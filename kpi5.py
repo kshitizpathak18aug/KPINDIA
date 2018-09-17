595a29bab9aedae67fd5b45a133d78a63cbf779c
kshitizpathak18aug <42990117+kshitizpathak18aug@users.noreply.github.com>
2018-09-17 11:41:28 +0530

    Add files via upload

---
+++ kpi5.py
@@ -0,0 +1,9 @@
+#find the greatest of two numbers
+a=int(input("Enter the value of a: "))
+b=int(input("Enter the value of b: "))
+if(a>b):
+	print("a is greater than b")
+if(a>b):
+	print("b is greater than a")
+else:
+	print("b is equal a")
