c74e74251adfc42af9771304cc3f35e98666699e
kshitizpathak18aug <42990117+kshitizpathak18aug@users.noreply.github.com>
2018-09-19 10:35:58 +0530

    Add files via upload

---
+++ asd23.py
@@ -0,0 +1,12 @@
+leaveprogram=0
+while leaveprogram != "q":
+	import random
+	print(random.randint(1,6))
+	print("This is dice rolling game")
+	print("Press k to roll")
+	input()
+	number=random.randint(1,6)
+	if number==1:
+		print()
+
+
