01f6db51b1830f7d0f9b94fb6f2cf629ee370bbb
kshitizpathak18aug <42990117+kshitizpathak18aug@users.noreply.github.com>
2018-09-28 09:52:58 +0530

    Add files via upload

---
+++ LABS7A.py
@@ -0,0 +1,50 @@
+import random
+
+print("Hello! Welcome to the Rock Paper Scissors Program!")
+
+champ= False
+
+while champ == False:
+    print("")
+    print("Press 1 for Rock.")
+    print("Press 2 for Paper.")
+    print("Press 3 for Scissors.")
+
+    PLAYER = int(input("What would you like to play?"))
+    ComputerInput = random.randint(1,3)
+
+    if (PLAYER == 1) and (ComputerInput == 1):
+        champ= False
+        print("It's a draw; you both played Rock!")
+        
+    if (PLAYER == 2) and (ComputerInput == 1):
+        champ == True
+        print("You win! The computer played Rock!")
+
+    if (PLAYER== 3) and (ComputerInput == 1):
+        champ== True
+        print("You lose! The computer played Rock!")
+        
+    if (PLAYER == 1) and (ComputerInput == 2):
+        champ = True
+        print("You lose! The computer played Paper!")
+        
+    if (PLAYER == 2) and (ComputerInput == 2):
+        champ== False
+        print("It's a draw; the computer played Paper!")
+
+    if (PLAYER == 3) and (ComputerInput == 2):
+        champ== True
+        print("You win! The computer played Paper!")
+        
+    if (PLAYER == 1) and (ComputerInput == 3):
+        champ= True
+        print("You win! The computer played Scissors!")
+        
+    if (PLAYER == 2) and (ComputerInput == 3):
+        champ == True
+        print("You lose! The computer played Scissors!")
+
+    if (PLAYER == 3) and (ComputerInput == 3):
+        champ == False
+        print("It's a draw; the computer played Scissors!")
